fc6b8924cff93d0812256ef79f5555f5a7c00da5
Pelayo García <pelayo.garcia@coopdevs.org>
2024-01-09 14:33:40 +0100

    Jan 9 [FIX] : Bug Fixed 'hide_cost_price'
    5-parts version in __manifest__.py

--- hide_cost_price/__manifest__.py
+++ hide_cost_price/__manifest__.py
@@ -24,7 +24,7 @@
 {
     'name': 'Hide Product Cost Price',
     'summary': """Product Cost Price Will be Visible Only for Specified Group""",
-    'version': '13.0.1.0',
+    'version': '13.0.1.0.0',
     'description': """Product cost price will be visible only for specified group""",
     'author': 'Cybrosys Techno Solutions',
     'company': 'Cybrosys Techno Solutions',
